d3c983f5f71c8800e608469c746e72fdf927f812
kaepa3_local <tak_3_zaki3@yahoo.co.jp>
2015-12-27 02:55:37 +0900

    add Files

---
+++ SoundLibrary/SoundLibrary/SoundLibrary.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Interop;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SoundLibrary
+{
+    public delegate void SoundEvent(int a);
+
+    public class SoundUtill
+    {
+        public SoundEvent PlayEvent;
+        string ariasName = "mySound";
+        public SoundUtill()
+        {
+        }
+        public SoundUtill(string text)
+        {
+            ariasName = text;
+        }
+        [DllImport("winmm.dll")]
+        extern static int mciSendString(string s1, StringBuilder s2, int i1, IntPtr i2);
+
+        [DllImport("winmm.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern MMRESULT waveOutWrite(IntPtr hwo, IntPtr pwh, uint cbwh);
+
+
+        /// <summary>
+        /// コールバックのデリゲート
+        /// </summary>
+        /// <param name="hdrvr"></param>
+        /// <param name="uMsg"></param>
+        /// <param name="dwUser"></param>
+        /// <param name="wavhdr"></param>
+        /// <param name="dwParam2"></param>
+        public delegate void WaveOutProc(IntPtr hdrvr, WaveOutMessage uMsg, int dwUser, IntPtr wavhdr, int dwParam2);
+
+        /// <summary>
+        /// ファイルオープン
+        /// </summary>
+        /// <param name="FileName"></param>
+        bool OpenFile(string fileName)
+        {
+            return 0 == mciSendString("open " + fileName + " alias " + ariasName, null, 0, IntPtr.Zero);
+        }
+
+        /// <summary>
+        /// クローズ処理
+        /// </summary>
+        bool CloseFile()
+        {
+            return 0 == mciSendString("close  " + ariasName, null, 0, IntPtr.Zero);
+        }
+        /// <summary>
+        /// 再生
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public bool Play(string fileName, Window wnd = null) 
+        {
+            if (OpenFile(fileName))
+            {
+                HwndSource source = wnd != null ? (HwndSource)HwndSource.FromVisual(wnd) : null;
+                if (mciSendString("play   " + ariasName, null, 0, source != null ? source.Handle : IntPtr.Zero) == 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        /// <summary>
+        /// 再生中か？
+        /// </summary>
+        /// <returns></returns>
+        public bool IsPlay()
+        {
+            StringBuilder sb = new StringBuilder(32);
+            if(mciSendString("status " + ariasName + " mode", sb, sb.Capacity, IntPtr.Zero) == 0)
+            {
+                return sb.ToString() == "playing";
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// 再生停止
+        /// </summary>
+        public bool PlayStop()
+        {
+            if (mciSendString("stop   " + ariasName, null, 0, IntPtr.Zero) == 0)
+            {
+                if (CloseFile())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public bool Rec()
+        {
+            bool flg = false;
+            mciSendString("open new type waveaudio alias Rec", null, 0, IntPtr.Zero);
+            
+            if (mciSendString("record Rec", null, 0, IntPtr.Zero) == 0)
+            {
+                flg =  true;
+            }
+            return flg;
+        }
+        /// <summary>
+        /// 録音終了
+        /// </summary>
+        /// <returns></returns>
+        public bool RecStop(string fileName)
+        {
+            try
+            {
+                mciSendString("stop Rec" , null, 0, IntPtr.Zero);
+                mciSendString("save Rec " + fileName, null, 0, IntPtr.Zero);
+                mciSendString("close Rec", null, 0, IntPtr.Zero);
+                return true;
+            }
+            catch{}
+            return false;
+
+        }
+    }
+}
---
+++ SoundLibrary/SoundLibraryTests1/SoundUtillTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SoundLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Diagnostics;
+
+namespace SoundLibrary.Tests
+{
+    [TestClass()]
+    public class SoundUtillTests
+    {
+
+        [TestMethod()]
+        public void PlayTest()
+        {
+            var obj = new SoundUtill("aho");
+            //  正常
+            Assert.AreEqual(true, obj.Play("test.wav"));
+
+            //  異常
+            Assert.AreEqual(false, obj.Play("tedsaf.wav"));
+        }
+
+        [TestMethod()]
+        public void StopTest()
+        {
+            var obj = new SoundUtill();
+            //  正常
+            Assert.AreEqual(true, obj.Play("test.wav"));
+            Assert.AreEqual(true, obj.PlayStop());
+            Assert.AreEqual(true, obj.Play("test.wav"));
+            Assert.AreEqual(true, obj.PlayStop());
+            //  ない場合。
+            Assert.AreEqual(false, obj.PlayStop());
+        }
+        [TestMethod()]
+        public void RecTest()
+        {
+            string str = "./result.wav";
+            var obj = new SoundUtill("faaaa");
+            //  正常
+            Assert.AreEqual(true, obj.Rec());
+            System.Threading.Thread.Sleep(4000);
+            Assert.AreEqual(true, obj.RecStop(str));
+            Assert.AreEqual(true, System.IO.File.Exists(str));
+
+            Debug.WriteLine("debug " + System.IO.Directory.GetCurrentDirectory());
+        }
+
+    }
+}
